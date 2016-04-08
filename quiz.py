d2af5a9e4e47e57c709b2d22e7054af28f37f932
Manwholikespie <rdominguez@me.com>
2016-04-07 20:11:47 -0500

    Update quiz.py
    Fixed the functions so that the script doesn't rely on global variables. Rather, I store quizlet's data in a single dictionary, and then use an xrange() function to loop through the terms, printing out their mneumonics with printAll()... I will admit, it was a bit bold using printAll() without even storing the information in a cache-esque list first, but you have to be bold sometimes. It's just the way of the samurai, if you will.

--- quiz.py
+++ quiz.py
@@ -2,16 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 
 # intialize lists for storing findings
-terms = []
-definitions = []
-mneumonics = []
-
 
 def printAll(listName):
     for item in listName:
         print(item)
 
 def getCards(inputUrl):
+    cardSet = {
+        'terms' : [],
+        'definitions' : []
+    }
+
     # set up beautiful soup
     r = requests.get(inputUrl)
     soup = BeautifulSoup(r.content, "html.parser")
@@ -19,18 +20,17 @@ def getCards(inputUrl):
     # pull term data
     termTag = soup.find_all("span",{"class": "TermText qWord lang-en"})
     for item in termTag:
-        terms.append(item.text)
+        cardSet['terms'].append(item.text)
 
     defTag = soup.find_all("span",{"class": "TermText qDef lang-en"})
     for item in defTag:
-        definitions.append(item.text)
+        cardSet['definitions'].append(item.text)
 
-    printAll(terms)
-    print("\n================================\n")
-    printAll(definitions)
-    print("\n================================\n")
+    return cardSet
 
 def getMneumonics(term):
+    mneumonics = []
+
     # set up beautiful soup
     url = "http://www.mnemonicdictionary.com/?word=" + term
     r = requests.get(url)
@@ -39,33 +39,37 @@ def getMneumonics(term):
     defTag = soup.find_all("div",{"class" : "span9"})
 
     if len(defTag) == 0:
-        print("No mneumonics for this word.")
+        return ['No mneumonics for this word.']
+    else:
+        for x in xrange(0,len(defTag)):
+            if x+1 == len(defTag):
+                mneumonics.append((defTag[x].text).replace("   ","").replace("\n","").replace("\r",""))
+                return mneumonics
+            elif x < 3:
+                mneumonics.append((defTag[x].text).replace("   ","").replace("\n","").replace("\r",""))
 
-    for x in xrange(0,len(defTag)):
-        tempMneumonic = ""
-        if x < 3:
-            print (defTag[x].text).replace("   ","").replace("\n","")
-            # removing some of this due to the fact that some definitions are
-            # encoded fairly strangely
-            # mneumonics.append(str(defTag[x].text).replace("   ","").replace("\n",""))
-        else:
-            mneumonics.append("\n")
-            break
 
 # ==========================  BEGIN MAIN FUNCTION   ============================
-# url = raw_input("Please paste the URL to the quizlet.\n > ")
+### FOR DEBUGGING:
+# url = "https://quizlet.com/247919/vocabulary-workshop-level-g-unit-13-flash-cards/"
+###
+print("""  ___        _     _
+ / _ \ _   _(_)___| |    ___  __ _ _ __ _ __
+| | | | | | | |_  / |   / _ \/ _` | '__| '_ \\
+| |_| | |_| | |/ /| |__|  __/ (_| | |  | | | |
+ \__\_\\\__,_|_/___|_____\___|\__,_|_|  |_| |_|
 
-# for the purpose of debugging, we shall have it be this URL for now.
-url = "https://quizlet.com/27467/sadlier-oxford-vocabulary-level-g-unit-11-flash-cards/"
-getCards(url)
-for x in xrange (0,len(terms)):
-    print(terms[x] + ":")
-    print(definitions[x])
+""")
+url = raw_input("Please paste the URL to the quizlet.\n > ")
+print("Searching...\n\n")
+
+cardSet = getCards(url)
+
+for x in xrange(0,len(cardSet['terms'])):
+    print(cardSet['terms'][x] + ":\n------------")
+    print(cardSet['definitions'][x] + "\n")
+    printAll(getMneumonics(cardSet['terms'][x]))
     print("\n")
-    getMneumonics(terms[x])
-    percent = float(x)/float(len(terms)) * 100
-    print("==============================")
-    # print(str(percent)+"%")
 
 print("\n\nThank you for using QuizLearn! Goodbye! :)")
 
